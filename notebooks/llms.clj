138e292d254c6c2720469ff722f70bb871a34088
daslu <daniel.slutsky@gmail.com>
2024-02-13 23:57:55 +0200

    llms chapter

--- notebooks/llms.clj
+++ notebooks/llms.clj
@@ -19,4 +19,65 @@
 
 (md "
 # LLMs: Using llama.clj
-")
+We will use [llama.clj](https://github.com/phronmophobic/llama.clj), a Clojure wrapper of llama.cpp.")
+
+;; ## Constants
+
+;; Path to the LLM:
+(def llama7b-path
+  (str (System/getenv "MODELS_PATH")
+       "/llama-2-7b-chat.ggmlv3.q4_0.bin"))
+
+;; One megabyte:
+(def MB (math/pow 2 20))
+
+;; ## Basics
+
+;; Creating a new model context:
+(defn new-llama-ctx []
+  (llama/create-context
+   llama7b-path
+   {:use-mlock true}))
+
+;; A copy of an empty model
+;; (to extract basic information):
+(def base-llama-ctx
+  (new-llama-ctx))
+
+;; A function to turn a String of text to a list of tokens:
+(defn tokenize [text]
+  (llutil/tokenize base-llama-ctx text))
+
+;; Example:
+(delay
+  (-> "The Fed says"
+      tokenize))
+
+;; A function to turn a list of tokens to a String of text:
+(defn untokenize [tokens]
+  (llutil/untokenize base-llama-ctx tokens))
+
+;; Example:
+(delay
+  (-> "The Fed says"
+      tokenize
+      untokenize))
+
+;; A map from tokens to the corresponding strings:
+(def token->str
+  (into (sorted-map)
+        (comp (map
+               (fn [token]
+                 [token (raw/llama_token_to_str base-llama-ctx token)]))
+              (take-while (fn [[token untoken]]
+                            untoken)))
+        (range 0 Integer/MAX_VALUE)))
+
+;; Example:
+(delay
+  (->> "The Fed says"
+       tokenize
+       (map token->str)))
+
+;; The EOS (end-of-sentence) token:
+(def llama-eos (llama/eos base-llama-ctx))
