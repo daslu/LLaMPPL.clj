d8a610a9a5f150d02a373601ee4bd5893b05e815
daslu <daniel.slutsky@gmail.com>
2024-02-14 23:43:19 +0200

    added example to preface

--- notebooks/index.clj
+++ notebooks/index.clj
@@ -1,7 +1,9 @@
 ^:kindly/hide-code
 (ns index
   (:require [scicloj.kindly.v4.kind :as kind]
-            [scicloj.kindly.v4.api :as kindly]))
+            [scicloj.kindly.v4.api :as kindly]
+            [llamppl.smc :as smc]
+            [tablecloth.api :as tc]))
 
 ^:kindly/hide-code
 (def md
@@ -21,4 +23,34 @@ by Alexander K. Lew, Tan Zhi-Xuan, Gabriel Grand, Vikash K. Mansinghka
 At the moment, we demonstrate implementing the Sequential Monte Carlo algoritm on a specific case, the Hard Constraints case generating texts with only short words with a certain choice of M (the Markove kernel) and G (the potential function), not the most efficient one.
 
 The main effort so far has been in tackling some of the caching challenges.
-")
+
+Our current bottom line is being able to generate
+a random population of texts (\"particles\" in SMC jargon)
+starting with \"The Fed says\"
+and using only words of at most 5 letters.")
+
+
+(require '[llamppl.llms :as llms]
+         '[llamppl.smc :as smc]
+         '[tablecloth.api :as tc])
+
+
+(delay
+  (let [*smc-state (atom (smc/new-smc-state))]
+    (smc/run-smc! *smc-state
+                  {:cache-threshold 30
+                   :seed 1
+                   :base-text "The Fed says"
+                   :max-n-letters 5
+                   :N 10
+                   :K 3
+                   :initial-N 5
+                   :max-text-length 10})
+    (-> @*smc-state
+        :particles
+        (tc/map-columns :finished [:x] llms/finished?)
+        (tc/map-columns :length [:x] count)
+        (tc/map-columns :text [:x] llms/untokenize)
+        (tc/drop-columns [:x])
+        (tc/set-dataset-name "texts")
+        (tech.v3.dataset.print/print-range :all))))
